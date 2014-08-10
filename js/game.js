ad12763f2eab8c97bd078d3f8a6be374c228c829
Giordon Stark <kratsg@gmail.com>
2014-08-10 14:26:06 -0500

    styling mostly. Built up rows of 3 columns representing the deck of cards. Columns are colored by the card color using an app.filter()

--- js/game.js
+++ js/game.js
@@ -3,14 +3,44 @@
 
   var app = angular.module('setApp', []);
 
-  app.controller('SetGameCtrl', ['$scope', function($scope) {
-    $scope.deck = Helpers.loadJSON('cards.json');
-    $scope.shuffleDeck = function(){
-      Helpers.shuffleArray($scope.deck);
-      console.log($scope.deck[0]);
+  //http://stackoverflow.com/questions/21644493/how-to-split-the-ng-repeat-data-with-three-columns-using-bootstrap
+  app.filter('partition', function() {
+    var cache = {};
+    var filter = function(arr, size) {
+      if (!arr) { return; }
+      var newArr = [];
+      for (var i=0; i<arr.length; i+=size) {
+        newArr.push(arr.slice(i, i+size));
+      }
+      var arrString = JSON.stringify(arr);
+      var fromCache = cache[arrString+size];
+      if (JSON.stringify(fromCache) === JSON.stringify(newArr)) {
+        return fromCache;
+      }
+      cache[arrString+size] = newArr;
+      return newArr;
+    };
+    return filter;
+  });
+
+  // why do I write filters this way? Probably to encapsulate code
+  app.filter('color', function(){
+    return function(num){
+      // '', 'red', 'purple', 'green'
+      var colors = ['','#CC0000','#CC00CC','#00CC00'];
+      return colors[num];
+    };
+  });
+
+
+  var SetGameCtrl = function(){
+    this.deck = Helpers.loadJSON('cards.json');
+    this.shuffleDeck = function(){
+      Helpers.shuffleArray(this.deck);
     }
-    console.log($scope.deck[0]);
-  }]);
+  }
+
+  app.controller('SetGameCtrl',SetGameCtrl);
 
 })();
 
